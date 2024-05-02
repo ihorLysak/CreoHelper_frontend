dc7a4fd0911f3e4e4cafd9646f8a4ee776c9cd37
ihorLysak <lysak.ihor4221@gmail.com>
2024-05-02 17:30:19 +0300

    began work on ChannelBtn component

--- src/components/channelBtn/ChannelBtn.tsx
+++ src/components/channelBtn/ChannelBtn.tsx
@@ -4,6 +4,12 @@ export function ChannelBtn() {
     return (
         <div className={styles.channelBtn}>
             <div className={styles.avatar}></div>
+            <span>Афіша Києва</span>
+            <div className={styles.stats}>
+                <span>5</span>
+                <span>+100</span>
+                <span>-25</span>
+            </div>
         </div>
     );
 }
