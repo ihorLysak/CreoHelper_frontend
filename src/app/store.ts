287c097cef3ebfe4b964b4b1ad1c040cbc256bc2
ihorLysak <lysak.ihor4221@gmail.com>
2024-10-02 16:58:35 +0300

    refactor: updated confib files

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sidebarReducer from '~/features/sidebar/sidebarSlice';
-
 export const store = configureStore({
     reducer: {
         sidebar: sidebarReducer,
